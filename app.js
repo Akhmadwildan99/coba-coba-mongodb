6c65efd3cf031e93ede844fdea035ed1dfe1b36a
Akhmad Wildan <wildanakhmad3584@gmail.com>
2021-06-29 14:29:12 +0700

    coba-coba mongodb

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const { MongoClient } = require("mongodb");
+const { MongoClient, ObjectID } = require("mongodb");
 
 const uri = 'mongodb://127.0.0.1:27017';
 const dbName = 'wildan';
@@ -20,18 +20,124 @@ client.connect((error, client) => {
 
     // Menambahkan 1 data ke collection mahasiswa
 
-    db.collection('mahasiswa').insertOne(
-        {
-            nama: 'izuku',
-            email: 'izuku@shonnen.com'
-        },
-        (error, result) => {
-            if(error){
-                return console.log('Gagal menambahkan data');
-            }
+    // db.collection('mahasiswa').insertOne(
+    //     {
+    //         nama: 'izuku',
+    //         email: 'izuku@shonnen.com'
+    //     },
+    //     (error, result) => {
+    //         if(error){
+    //             return console.log('Gagal menambahkan data');
+    //         }
 
-            console.log(result)
-        }
-    );
+    //         console.log(result)
+    //     }
+    // );
+
+    // Menambahkan lebih dari 1 data
+
+    // db.collection('mahasiswa').insertMany(
+    //     [
+    //         {
+    //             nama: 'izuku',
+    //             email: 'izuku@deku.co.id',
+    //         },
+    //         {
+    //             nama: 'roland',
+    //             email: 'rolando@gmail.com',
+    //         },
+    //     ],
+    //     (error, result) => {
+    //         if(error) {
+    //             return console.log('Gagal menambahkan data!');
+    //         }
+
+    //         console.log(result);
+    //     }
+    // );
+
+    // // Menampilkan semua data di collections mahasiswa
+
+    // console.log(
+    //     db.collection('mahasiswa')
+    //     .find()
+    //     .toArray((error, result)=>{
+    //         console.log(result);
+    //     })
+    // );
+
+
+    // Menampilkan data berdasarkan kriteria di collections mahasiswa
+
+    // console.log(
+    //     db.collection('mahasiswa')
+    //     .find({ _id: ObjectID('60dabf25ddbaa11a5c6dd325') })
+    //     .toArray((error, result)=>{
+    //         console.log(result);
+    //     })
+    // );
+
+
+    // Update data berdasarkan id
+
+    // const updatePromise = db.collection('mahasiswa').updateOne(
+    //     {
+    //         _id: ObjectID("60d9c8d70ba06d62d57f7f93"),
+    //     },
+    //     {
+    //         $set: {
+    //             nama: 'William',
+    //         }
+    //     }
+    // );
+
+    // updatePromise.then((result)=>{
+    //     console.log(result);
+    // }).catch((error) => {
+    //     console.log(error);
+    // });
+
+    // Mengubah data lebih dari 1 berdasarkan kriteria
+
+    // db.collection('mahasiswa').updateMany(
+    //     {
+    //         nama: 'izuku',
+    //     },
+    //     {
+    //         $set: {
+    //             nama: 'izuku midoriya shonnen',
+    //         }
+    //     }
+    // );
+
+
+    // // Menghapus 1 data
+
+    // db.collection('mahasiswa')
+    // .deleteOne(
+    //     {
+    //         _id: ObjectID('60dabf25ddbaa11a5c6dd325'),
+    //     }
+    // )
+    // .then((result)=>{
+    //     console.log(result);
+    // }).catch((error)=>{
+    //     console.log(error);
+    // });
+
+
+     // Menghapus lebih dari 1 data
+
+     db.collection('mahasiswa')
+     .deleteMany(
+         {
+             nama: 'izuku midoriya shonnen',
+         }
+     )
+     .then((result)=>{
+         console.log(result);
+     }).catch((error)=>{
+         console.log(error);
+     });
 
 });
